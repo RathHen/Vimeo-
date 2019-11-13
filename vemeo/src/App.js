4084ba20becca999dfb2a5a7772051fa96165012
42045764 <rathhen123@gmail.com>
2019-11-13 04:39:08 -0500

    Part 2 Implemented

--- vemeo/src/App.js
+++ vemeo/src/App.js
@@ -1,76 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './index.css';
 
 function App() {
-  const wrapper = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    paddingTop: '100px',
-    paddingBottom: '100px',
-    // marginLeft: '5%',
-    flexWrap: 'wrap-reverse',
-  };
-
-  const beams = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    paddingTop: '100px',
-    paddingBottom: '100px',
-    flexWrap: 'wrap',
-    background: '#575757'
-
-  };
-  const Move2 = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    paddingTop: '100px',
-    paddingBottom: '100px',
-    flexWrap: 'wrap',
-    backgroundImage: 'linear-gradient(grey, white)'
-
-  };
-
-
-
 
   return (
     <div>
-      <div style={wrapper}>
-
+      {/* Div container for Monsoon */}
+      <div className="wrapperTwo">
             <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg"
                  alt="new"
-                 className="center"
-            />
-
+                 className="center"/>
             <div style={{textAlign: 'left', width: '30%',  paddingLeft: "30px" }}>
-              <h2  > MONSOON III
-              </h2>
-              <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
-              dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
-              Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
-              sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
-              </p>
+                <h2  > MONSOON III</h2>
+                <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+                dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+                Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+                sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
+                </p>
             </div>
       </div>
 
-
-        <div style={beams}>
-
+      <div className="beams">
+          {/* Div container for Beams */}
           <div style={{textAlign: 'left', width: '35%', paddingRight: "30px", color: 'white' }}>
-            <h2 style={{fontColor: 'white'}}> BEAMS
-            </h2>
+            <h2 style={{fontColor: 'white'}}> BEAMS</h2>
             <p>
               Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
               dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
@@ -80,23 +35,16 @@ function App() {
           </div>
           <img src="https://i.vimeocdn.com/video/589972810_530x315.jpg"
                alt="new"
-               className="center"
-          />
+               className="center"/>
+      </div>
 
-        </div>
-
-
-
-
-
-      <div style={Move2}>
+      <div className="Move2">
+        {/* Div container for Move2 */}
         <img src="https://i.vimeocdn.com/video/590587169_530x315.jpg"
              alt="new"
-             className="center"
-        />
+             className="center"/>
         <div style={{textAlign: 'left', width: '35%', paddingLeft: "30px" , color: 'white'}}>
-          <h2  > MOVE 2
-          </h2>
+          <h2  > MOVE 2</h2>
           <p>
             Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
             dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
@@ -104,12 +52,7 @@ function App() {
             sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
           </p>
         </div>
-
       </div>
-
-
-
-
     </div>
   );
 }
---
+++ vemeo/src/Carousel.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import './App.css';
+
+
+
+const content = [
+    {
+        title: 'Hunt for the WilderPeople',
+        description:
+            'Raised on hip-hop and foster case, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We Do In The Shadows.',
+        button: 'Buy Now',
+        buttonTwo: 'Watch Trailer',
+        image: 'https://in.bmscdn.com/iedb/movies/images/website/poster/large/hunt-for-the-wilderpeople-et00044767-17-04-2017-22-36-41.jpg',
+        background: "background1"
+
+
+    },
+    {
+        title: 'Nights Passiert',
+        description:
+            'A Swiss family takes a ski vacation and runs into trouble when the father, the titular decent man, finds himself in a series of moral quandaries.',
+        button: 'Buy Now',
+        buttonTwo: 'Watch Trailer',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Nichtspassiertplakat_CC.jpg',
+        background: "background2"
+
+
+    },
+    {
+        title: 'Vice Versa',
+        description:
+            'Come along with the Good Company crew as they travel throughout the US, Japan, BC, and Quebec to showcase skiing in the best way possible.',
+        button: 'Buy now',
+        buttonTwo: 'Watch Trailer',
+        image: 'https://is3-ssl.mzstatic.com/image/thumb/Video62/v4/35/b9/a2/35b9a22f-6c95-7977-ebda-252245aa4688/pr_source.png/268x0w.png',
+        background: "background3"
+
+    },
+    {
+        title: 'The Fourth Phase',
+        description:
+            'From the creators of The Art of Flight, Red Bull Media House presents THE FOURTH PHASE, a snowboarding epic starring iconic athletes Travis Rice.',
+        button: 'Buy Now',
+        buttonTwo: 'Watch Trailer',
+        image: 'http://www.gstatic.com/tv/thumb/v22vodart/12392207/p12392207_v_v8_aa.jpg',
+        background: "background4"
+
+    },
+    {
+        title: 'Full Moon',
+        description:
+            'Be inspired by women who push boundaries. FULL MOON showcases the legends, current icons and future prodigies of this ever-evolving lifestyle sport.',
+        button: 'Buy Now',
+        buttonTwo: 'Watch Trailer',
+        image: 'https://ksr-ugc.imgix.net/assets/012/216/351/dec5b95922645c2f247e200a03b32a26_original.jpg?ixlib=rb-2.1.0&crop=faces&w=1552&h=873&fit=crop&v=1463743069&auto=format&frame=1&q=92&s=de7d2d8d065f1e35d3b13dacd2328606',
+        background: "background5"
+
+    }
+];
+
+function onClick() {
+    window.location.href="https://play.google.com/store/movies?hl=en_US";
+}
+
+function onClickTwo() {
+    window.location.href="https://www.rottentomatoes.com/trailers/";
+}
+
+
+
+function Carousel() {
+
+
+    return(
+        <div>
+            <Slider>
+                {content.map((item, index) => (
+                    <div
+                        key={index}
+                        className="slider-content">
+                        <div className={item.background}></div>
+                        <div className="wrapper">
+                            <img style={{ width: '15%',
+                                height: '15%',alignItems: 'center',}} src={item.image}
+                                 alt="new"
+                                 className="center"/>
+                            <div style={{textAlign: 'left', width: '40%',  paddingLeft: "30px", paddingBottom: "10%", flexWrap: 'nowrap' }}>
+                                <h2 style={{color: 'white', fontFamily: "Arial"}}>{item.title}</h2>
+                                <p style={{color: 'white', fontFamily: "Courier"}}>{item.description}</p>
+                                <button id="bt" className="buttonStyleOne" onClick={onClick}>{item.button}</button>
+                                <button id="bt2" className="buttonStyleTwo" onClick={onClickTwo}>{item.buttonTwo}</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+}
+export default Carousel;
